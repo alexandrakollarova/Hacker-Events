58df82a98879e54209c593dc734bbc40fecf8002
Alexandra Kollarova <alexandrakollarova91@gmail.com>
2019-07-11 11:03:13 -0400

    app in progress

--- script.js
+++ script.js
@@ -1,23 +1,21 @@
 "use strict"
 
+function getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition);
+    } else { 
+      x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
 
-// function getLocation() {
-//     if (navigator.geolocation) {
-//       navigator.geolocation.getCurrentPosition(usePosition);
-//     } else { 
-//       x.innerHTML = "Geolocation is not supported by this browser.";
-//     }
-// }
+$(getLocation);
 
-// function usePosition(position) {
-//     const latitude = position.coords.latitude;
-//     const longitude = position.coords.longitude;
-// }
+function showPosition(position) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+}
 
-// $(getLocation);
-
-
-const searchURL = "https://www.eventbriteapi.com/v3/events/search/?q=hackathons&location.address=boston";
+const searchURL = "https://www.eventbriteapi.com/v3/events/search/?q=hackathons&location.latitude=" + latitude + "&location.longitude=" + longitude;
 
 function formatQueryParams(params) {
     const queryItems = Object.keys(params).map(key => 
@@ -28,6 +26,25 @@ function formatQueryParams(params) {
 
 function displayResults(responseJson) {
     console.log(responseJson);
+
+    for (let i = 0; i < responseJson.events.length; i++) {
+        $("#js-search-results").append(
+            `<section class="event-card">
+                <a href="${responseJson.events[i].url}">
+                    <div class="card">
+                        <img src="${responseJson.events[i].logo.original.url}">
+
+                        <div class="container">
+                            <h2>${responseJson.events[i].name.text}</h2> 
+                            <h5>${responseJson.location.augmented_location.city}</h5>                            
+                            <h5>${responseJson.events[i].start.local}</h5>                            
+                            <h5>${responseJson.events[i].end.local}</h5>
+                        </div>
+                    </div>
+                </a>
+            </section>`
+        )
+    }    
 }
 
 function getEvents(query) {
@@ -53,7 +70,7 @@ function getEvents(query) {
             }
             throw new Error(response.statusText);
         })
-        .then(responseJson => console.log(responseJson))
+        .then(responseJson => displayResults(responseJson))
         .catch(err => {
             $("#js-error-message").text(`Something went wrong: ${err.message}`);
         });
