8175c733a29af47f217ec2f78bda79d4b9c26515
Alexandra Kollarova <alexandrakollarova91@gmail.com>
2019-07-18 11:16:52 -0400

    working on Youtube API

--- script.js
+++ script.js
@@ -134,62 +134,73 @@ function displayResultsFromEventBrite(responseJson) {
     }        
 }
 
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// 3. This function creates an <iframe> (and YouTube player)
+//    after the API code downloads.
+var player;
+function onYouTubeIframeAPIReady(responseJson) {
+
+    for (let i = 0; i <= responseJson.items.length; i++) {
+        
+        player = new YT.Player('player1', {
+            height: '390',
+            width: '640',
+            videoId: responseJson.items[0].id.videoId,
+            playerVars: {
+                autoplay: 0,
+            },
+            events: {
+                'onReady': onPlayerReady,
+                'onStateChange': onPlayerStateChange
+            }
+        });
+
+        player = new YT.Player('player2', {
+            height: '390',
+            width: '640',
+            videoId: responseJson.items[1].id.videoId,            
+            playerVars: {
+                autoplay: 0,
+            },
+            events: {
+                'onReady': onPlayerReady,
+                'onStateChange': onPlayerStateChange
+            }
+        });
+    }
+}
+
+// 4. The API will call this function when the video player is ready.
+function onPlayerReady(event) {
+    event.target.playVideo();
+}
+
+// 5. The API calls this function when the player's state changes.
+//    The function indicates that when playing a video (state=1),
+//    the player should play for six seconds and then stop.
+var done = false;
+function onPlayerStateChange(event) {
+    if (event.data == YT.PlayerState.PLAYING && !done) {
+        setTimeout(stopVideo, 6000);
+        done = true;
+    }
+}
+
+function stopVideo() {
+    player.stopVideo();
+}
+
 
       
 function displayResultsFromYoutube(responseJson) {
     console.log(responseJson);
-    var tag = document.createElement('script');
-
-    tag.src = "https://www.youtube.com/iframe_api";
-    var firstScriptTag = document.getElementsByTagName('script')[0];
-    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-    // 3. This function creates an <iframe> (and YouTube player)
-    //    after the API code downloads.
-    var player;
-    function onYouTubeIframeAPIReady() {
-      player = new YT.Player('player', {
-        height: '390',
-        width: '640',
-        videoId: 'M7lc1UVf-VE',
-        events: {
-          'onReady': onPlayerReady,
-          'onStateChange': onPlayerStateChange
-        }
-      });
-    }
-
-    // 4. The API will call this function when the video player is ready.
-    function onPlayerReady(event) {
-      event.target.playVideo();
-    }
-
-    // 5. The API calls this function when the player's state changes.
-    //    The function indicates that when playing a video (state=1),
-    //    the player should play for six seconds and then stop.
-    var done = false;
-    function onPlayerStateChange(event) {
-      if (event.data == YT.PlayerState.PLAYING && !done) {
-        setTimeout(stopVideo, 6000);
-        done = true;
-      }
-    }
-    function stopVideo() {
-      player.stopVideo();
-    }
-
-    //    for (let i = 0; i <= responseJson.items.length; i++) {
-
-    //         player.loadPlaylist({
-    //             list: responseJson.items[i].id.videoId,
-    //             listType: 'search',
-    //             index: 0,
-    //             startSeconds: 0,
-    //             suggestedQuality: 'high'
-    //         })
-    //     }
+    
         
-
 
     // for (let i = 0; i <= responseJson.items.length; i++) {
 
@@ -297,7 +308,7 @@ function getVideosFromYoutube() {
             }
             throw new Error(response.statusText);
         })
-        .then(responseJson => displayResultsFromYoutube(responseJson))
+        .then(responseJson => onYouTubeIframeAPIReady(responseJson))
         .catch(err => {
             $("#js-youtube-error-message").text(`Something went wrong: ${err.message}`);
         });
